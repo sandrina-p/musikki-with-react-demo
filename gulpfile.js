c21f2416f109bdcbbd96c770f650650b34360c61
sandrina-p <sandrina.pereira@zaask.com>
2016-11-25 19:40:02 +0000

    clean gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,28 @@ var gulp = require('gulp'),
     argv = require('yargs').argv, // flags to command-line
     gulpif = require('gulp-if'), // execute some task conditions (ex dev vs prod)
     gutil = require('gulp-util'), // better error logs like pump
-    changed = require('gulp-changed'), // NOTE: only compiles what was changed
-    cached = require('gulp-cached'), // NOTE: i'm not sure if this is working correctly
+    changed = require('gulp-changed'), // only compiles what was changed
+    cached = require('gulp-cached'),
     stripDebug = require('gulp-strip-debug'), //bye bye console.logs
-    chalk = require('chalk'), //because after all i'm a designer and i need some colors on terminal x)
+    chalk = require('chalk'), //some color on terminal
 
-    php2html = require("gulp-php2html"), //turn php into static files to upload on github
-
-    browserSync = require('browser-sync'), // i'm not sure how this and connect-php works ...
-    php = require('gulp-connect-php'), // ... but you can read more about it here -> http://stackoverflow.com/a/37040763/4737729
-    reload = browserSync.reload;
+    browserSync = require('browser-sync'),
 
     //scss stuff
     sass = require('gulp-sass'),
     cleanCSS = require('gulp-clean-css'),
     autoprefixer = require('gulp-autoprefixer'),
     sassInheritance = require('gulp-sass-multi-inheritance'), // watch partials
-    importCss = require('gulp-import-css'), //combine css imports (not scss)
 
     //javascript plugins
     uglify = require('gulp-uglify'),
     include = require("gulp-include"), //append js
     babel = require('gulp-babel'); // ES2015 rocks
 
+
 var folderScripts = 'assets/scripts',
     folderStyles = 'assets/styles',
     srcScss = [ folderStyles+'/**/*.scss', '!'+folderStyles+'/**/_*.scss'];
-
 
 
 function logEnv() {
@@ -56,9 +51,6 @@ gulp.task('scripts', function(){
         }))
 
         .pipe(gulpif(argv.production, stripDebug()))
-        // .pipe(gulpif(argv.production,
-        //     uglify().on('error', gutil.log)
-        // ))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulpif(global.isWatching, cached('cachedPlace')))
         .pipe(gulp.dest( function(file) { return file.base; } ));
@@ -76,7 +68,6 @@ gulp.task('scss', function(){
             browsers: ['last 2 version', 'safari 6', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
             cascade: true
         }))
-        .pipe(importCss())
         .pipe(gulpif(argv.production,
             cleanCSS({
                 compatibility: 'ie9'
@@ -104,18 +95,8 @@ gulp.task('scssPartials', function() {
 
 gulp.task('min-all', ['scripts', 'scss'] );
 
-gulp.task('gen-html', function(){
-    gulp.src("index.php")
-    .pipe(php2html())
-    .pipe(gulp.dest(""));
-});
 
-
-gulp.task('php', function() {
-    php.server({ base: 'http://localhost:8888/git/s008080', port: 8888, keepalive: true});
-});
-
-gulp.task('browser-sync',['php'], function() {
+gulp.task('browser-sync', function() {
     browserSync({
         server: {
             baseDir: './'
@@ -142,12 +123,6 @@ gulp.task('watch', ['setWatch', 'scssPartials', 'browser-sync'], function(){
     gulp.watch(['**/*.php'], ['gen-html']);
 });
 
-gulp.task('serverr', ['setWatch', 'scssPartials', 'browser-sync'], function(){
-    gulp.watch(['**/*.php'], ['gen-html']);
-});
-
-
-
 gulp.task('tasks', function(){
         var chTitle = chalk.bold.blue;
         var chBold = chalk.bold;
@@ -164,22 +139,8 @@ gulp.task('tasks', function(){
             +"\n"+chTitle('$ gulp styles')+"\n"
             	+"     compile and minify all scss (!_*.scss) on assets/styles to .min.css \n"
 
-            // +"\n"+chTitle('$ gulp min-this --js [path/to/file.js]')+"\n"
-            // 	+"     minify only a specific .js file. Useful for js on assets/plugins \n"
-            //
-            // +"\n"+chTitle('$ gulp min-this --scss [path/to/file.scss]')+"\n"
-            // 	+"     compile only a specific .scss file. Useful for scss on assets/plugins \n"
-            //
-            // +"\n"+chTitle('$ gulp min-this --jsscss [path/to/folder]')+"\n"
-            // 	+"     compile all .js files inside that specific folder. \n"
-            //
-            // +"\n"+chTitle('$ gulp min-this --scssfolder [path/to/folder]')+"\n"
-            // 	+"     compile all .scss files inside that specific folder. \n"
-
             +"\n"+chTitle('$ gulp min-all')+"\n"
             	+"     run scripts and styles tasks. use '--production' to also minify them.\n"
-            +"\n"+chTitle('$ gulp gen-html')+"\n"
-            	+"     compile index.php to index.html. because bitbucket doesn't like php.\n"
 
         );
 });
